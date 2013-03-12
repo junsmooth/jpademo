a1329c8546e83b4b26893a28f1490626a5603f3b
Administrator <Administrator@PC-201209221127>
2013-03-12 16:27:11 +0800

    dd

--- jpa/src/main/java/org/bgrimm/jpa/service/UserDaoImpl.java
+++ jpa/src/main/java/org/bgrimm/jpa/service/UserDaoImpl.java
@@ -1,33 +1,19 @@
 package org.bgrimm.jpa.service;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceUnit;
+import javax.persistence.PersistenceContext;
 
 import org.bgrimm.jpa.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public class UserDaoImpl {
-//	 @Resource(name = "entityManagerFactory")
-//	 protected EntityManagerFactory emf;
+	@PersistenceContext
+	private EntityManager entityManager;
 
-	
-	@PersistenceUnit
-	private EntityManager emf;
-
-//	@PersistenceUnit
-//	public void setEntityManagerFactory(EntityManagerFactory emf) {
-//		this.emf = emf;
-//		System.out.println("aaaaa"+emf.getProperties());
-//	}
-
-	@Transactional(propagation=Propagation.REQUIRED, readOnly=false)
 	public void saveUser(User user) {
-		System.out.println("bbbbb"+emf);
-		emf.persist(user);
+		entityManager.persist(user);
 	}
 }
